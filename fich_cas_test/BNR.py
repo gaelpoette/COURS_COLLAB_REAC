e5a45d9f12f4d2ec6fa11f27a04a6de48bef7322
Poette <gael.poette@gmail.com>
2023-10-30 10:36:30 +0100

    mise en place de tests

---
+++ fich_cas_test/BNR.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from math import *
+from string import *
+import os
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+os.chdir("BNR/test_repro")
+os.system("cp param.py ../../..")
+os.system("python3 ../../../ode_mc.py")
