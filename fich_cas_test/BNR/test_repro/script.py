47e6fac3d84697ac8795b80dce5c52d468c0670c
Brieuc Praud <brieucpraud@gmail.com>
2023-11-27 10:13:39 +0100

    fonctionnalité flêche pour l'équation bilan

---
+++ fich_cas_test/BNR/test_repro/script.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from math import *
+from string import *
+import os
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+os.system("cp param.py ../../..")
+os.system("python3 ../../../ode_mc.py")
+os.system("cp rez.txt rez1.txt")
+os.system("python3 ../../../ode_mc.py")
+os.system("meld rez.txt rez1.txt")
+os.system("rm -f rez.txt rez1.txt gnu.plot")
