3049cdbc872d9737fac90560c1aefaa0fd232e42
Poette <gael.poette@gmail.com>
2023-12-04 10:38:25 +0100

    retrait lignes qui ne servent à rien

--- fich_cas_test/BNR.py
+++ fich_cas_test/BNR.py
@@ -10,11 +10,10 @@ import numpy as np
 
 
 os.chdir("BNR/test_repro")
-os.system("cp param.py ../../..")
 os.system("python3 script.py")
 os.chdir("../..")
 
 os.chdir("BNR/comp_codes")
-os.system("cp param.py ../../..")
 os.system("python3 script.py")
+os.chdir("../..")
 
