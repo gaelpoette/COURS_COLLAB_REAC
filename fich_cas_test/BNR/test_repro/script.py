fa1aebfc9e63d083b39c7cbf0b049df64b2fbf2b
00mariam00 <114618332+00mariam00@users.noreply.github.com>
2023-12-04 10:43:32 +0100

    script de comparaison et clean.py

--- fich_cas_test/BNR/test_repro/script.py
+++ fich_cas_test/BNR/test_repro/script.py
@@ -8,11 +8,20 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
+def compare_files(file1, file2):
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        if f1.read() == f2.read():
+            return "The tests are identical"
+        else:
+            return "The tests are not identical"
 
 os.system("cp param.py ../../..")
 os.system("python3 ../../../ode_mc.py")
 os.system("cp rez.txt rez1.txt")
 os.system("python3 ../../../ode_mc.py")
-os.system("meld rez.txt rez1.txt")
+comparison_result = compare_files("rez.txt", "rez1.txt")
+
+#os.system("meld rez.txt rez1.txt")
 #os.system("diff rez.txt rez1.txt")
 os.system("rm -f rez.txt rez1.txt gnu.plot")
+#print("\n----------\n" + comparison_result + "\n----------\n")
