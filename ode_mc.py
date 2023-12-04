07e0aa0df54af416b67ffaad82d8f1cf3c543669
CLAVELIN Samuel <sclavelin@enseirb-matmeca.fr>
2023-12-04 10:03:31 +0100

    :/

--- ode_mc.py
+++ ode_mc.py
@@ -12,12 +12,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from fonction import * 
 
-random.seed(49)  # Fixe la graine à la valeur 42
+random.seed(42)  # Fixe la graine à la valeur 42
 
 # importation des paramètres
+from param import *
+
 if(sig_r_0<0 or sig_r_1<0 or sig_r_2<0):
     print("ATTENTION! Les constantes de reaction doivent etre positif")
     exit(1)
+
+#importation fonction tirage loi exponentielle
+from tirage_exp import *
 
 print("liste des reactions")
 print(list_reac)
@@ -26,30 +31,82 @@ if (not(len(list_reac)==len(list_sigr))):
   print("ATTENTION! LES LISTES DOIVENT AVOIR LA MEME TAILLE!")
   exit(1)
 
-# Traiter le cas où les constantes de réaction sont négatives (Jabir)
-for i in range (len(list_sigr)):
-    if (list_sigr[i]<0):
-      print("Une des constantes de réaction est négative")
-      list_sigr[i]= abs(list_sigr[i])
+# lecture de la liste des compositions des réactions
+compos=[]
+for i in range(len(list_reac)): 
+  compos_reac=(list_reac[i].split(' '))
+  for j in range(len(compos_reac)):
+     if not(compos_reac[j] in compos):
+       compos.append(compos_reac[j])
 
+print("liste des especes")
+print(compos)
 
 #"conditions initiales en eta codée en dur pour l'instant
-# remlissage de compos et initialisation des vecteurs eta, h et nu
-eta, hn, nu, compos = vector_init(list_reac, list_type, vol)
+eta={}
+for c in compos:
+    eta[c]=0.
+    if c=="Ar" or c=="e^-":
+      eta[c] = 1. * vol
+	
+print("conditions initiales des espèces")
+print(eta)
 
+h={}
+nu={}
+for i in range(len(list_reac)):
+    print("\n num de reaction = "+str(i)+"")
+    reac = list_reac[i]
+    compos_reac = (reac.split(' '))
+    print(compos_reac)
+    # recuperation du vecteur des reactifs
+    print("type de reaction: "+list_type[i]+"")
+
+    isnum=0
+    if list_type[i] == "binaire":
+          h[i] = [compos_reac[0], compos_reac[1]]
+    elif list_type[i] == "unaire":
+          h[i] = [compos_reac[0]]
+    else:
+          print("type de reaction non reconnue")
+          exit(2)
+
+    #recuperation des vecteurs de coefficients stoechiométriques pour chaque reactions
+    nu[i]={}
+    #print compos
+    for cg in compos:
+        nu[i][cg] = 0.
+        num = 0
+        for c in compos_reac:
+          isnum=0
+          if list_type[i] == "binaire":
+              isnum = (num == 0 or num == 1)
+          if list_type[i] == "unaire":
+              isnum = (num == 0)
+          if c == cg and (isnum): #réactions à 2 réactifs
+              nu[i][cg] += -1.
+          if c == cg and (not isnum): #réactions à 2 réactifs
+              nu[i][cg] +=  1.
+          else:
+              nu[i][cg] +=  0.
+          num+=1
+print("\nles listes de réactifs (h) pour chaque reaction")
+print(h)
+print("les coefficients stoechiométriques (nu) pour chaque reaction")
+print(nu)
 # population de particules représentant la condition initiale
 PMC = pmc_init(Nmc, compos, eta)
 
 #entete du fichier
 cmd="\n"+"#temps"+" "
-for ci in compos:
- cmd+=str(ci)+" "
+for c in compos:
+ cmd+=str(c)+" "
 
 it=0
 tps = 0.
 cmd+="\n"+str(tps)+" "
-for ci in compos:
- cmd+=str(eta[ci]/vol)+" "
+for c in compos:
+ cmd+=str(eta[c]/vol)+" "
 
 print("\n début du calcul")
 
@@ -58,8 +115,8 @@ while tps < temps_final:
   dt = temps[it+1]-temps[it]
 
   # initialisation du tableau de tallies
-  for ci in compos:
-      eta[ci] = 0.
+  for c in compos:
+      eta[c] = 0.
 
   for pmc in PMC:
     
@@ -69,60 +126,51 @@ while tps < temps_final:
 
           # section efficace totale
           sig = 0.
-          for it in range(len(list_reac)):
+          for i in range(len(list_reac)):
               prod = 1.
-              for Hi in hn[it]:
-                  prod *= pmc["densities"][Hi]
+              for H in h[i]:
+                  prod *= pmc["densities"][H]
 
               exposant = 1
-              if list_type[it] == "unaire":
+              if list_type[i] == "unaire":
                   exposant = 0
               volr = vol **exposant
-              sig+= list_sigr[it] / volr * prod
+              sig+= list_sigr[i] / volr * prod
 
-          #tirage du temps de la prochaine reaction
-          Urand = random.random()
-          tau = 1.e32
-          if sig > 0.:
-              tau = - log(Urand) / sig
 
-          # temps courant updaté
-          tps_cur += tau
+          # temps courant updaté, tau_func revoie tau
+          tps_cur += tau_func(sig)
 
           # détermination de l'évenement que la pmc va subir
           if tps_cur > dt:
               #census
               tps_cur = dt
-              for ci in compos:
-                  eta[ci] += pmc["densities"][ci] * pmc["weight"]
+              for c in compos:
+                  eta[c] += pmc["densities"][c] * pmc["weight"]
 
           else:
-            # Fonction de tirages de réactions:
-            def reaction(list_reac, hn, list_type, list_sigr, sig):
-                U = random.random()
+              #reaction
+              U = random.random()
 
-                reac = len(list_reac)-1
-                proba = 0.
-                for i in range(len(list_reac)-1):
-                    prod = 1.
-                    for H in hn[i]:
-                        prod *= pmc["densities"][H]
+              reac = len(list_reac)-1
+              proba = 0.
+              for i in range(len(list_reac)-1):
+                  prod = 1.
+                  for H in h[i]:
+                      prod *= pmc["densities"][H]
 
-                    exposant = 1
-                    if list_type[i] == "unaire":
-                        exposant = 0
-                    volr = vol **exposant
-                    proba+= list_sigr[i] / volr * prod
+                  exposant = 1
+                  if list_type[i] == "unaire":
+                      exposant = 0
+                  volr = vol **exposant
+                  proba+= list_sigr[i] / volr * prod
 
-                    if U * sig < proba:
-                        reac = i
-                        break
+                  if U * sig < proba:
+                      reac = i
+                      break
 
-                return reac
-
-            reac = reaction(list_reac, hn, list_type, list_sigr, sig) 
-            for c in compos:
-                  pmc["densities"][c]+=nu[reac][c]         
+              for c in compos:
+                  pmc["densities"][c]+=nu[reac][c]
 
   tps+=dt
   cmdt=""+str(tps)+" "
@@ -134,45 +182,18 @@ output = open("rez.txt",'w')
 output.write(cmd)
 output.close()
 
+cmd_gnu="set sty da l;set grid; set xl 'time'; set yl 'densities of the species'; plot "
+i=3
+cmd_gnu+="'rez.txt' lt 1 w lp  t '"+str(compos[0])+"'"
+for c in compos:
+   if not(c==compos[0]):
+     cmd_gnu+=",'' u 1:"+str(i)+" lt "+str(i)+" w lp t '"+str(compos[i-2])+"'"
+     i+=1
 
-######################### Plot GNU ###################################
+cmd_gnu+=";pause -1"
+output = open("gnu.plot",'w')
+output.write(cmd_gnu)
+output.close()
 
-if gnuplot:
-    cmd_gnu="set sty da l;set grid; set xl 'time'; set yl 'densities of the species'; plot "
-    it=3
-    cmd_gnu+="'rez.txt' lt 1 w lp  t '"+str(compos[0])+"'"
-    for c in compos:
-        if not(c==compos[0]):
-            cmd_gnu+=",'' u 1:"+str(it)+" lt "+str(it)+" w lp t '"+str(compos[it-2])+"'"
-            it+=1
-
-    cmd_gnu+=";pause -1"
-    output = open("gnu.plot",'w')
-    output.write(cmd_gnu)
-    output.close()
-
-    os.system("gnuplot gnu.plot")
-
-######################### Plot Matplotlib ############################
-
-else:
-    # Charger les données depuis 'rez.txt'
-    data = np.loadtxt('rez.txt')
-
-    # Créer une figure et un axe
-    fig, ax = plt.subplots()
-
-    # Configurer l'axe
-    ax.set_xlabel('time')
-    ax.set_ylabel('densities of the species')
-    ax.grid(True)
-    # Dessiner chaque colonne de données en tant que série distincte
-    for it, c in enumerate(compos):
-        ax.plot(data[:, 0], data[:, it+1], 'o-', label=c)
-
-    # Afficher la légende
-    ax.legend(loc='best')
-
-    # Enregistrer la figure en tant qu'image
-    fig.savefig('output.png')
+os.system("gnuplot gnu.plot")
 
