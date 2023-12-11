719b1beaf963dac8570ab033c101fd5ca7d41cc9
Poette <gael.poette@gmail.com>
2023-12-11 09:29:04 +0100

    Ajout de \n dans la reference du cas test (merci Antoine)

--- fich_cas_test/BNR/test_exception_sig_neg/test.py
+++ fich_cas_test/BNR/test_exception_sig_neg/test.py
@@ -14,9 +14,11 @@ os.system("cp param.py ../../..")
 os.system("python3 ../../../ode_mc.py > listing")
 
 f = open("listing", 'r')
+ref = "ATTENTION! Les constantes de reaction doivent etre positif\n"
 for line in f:
     #print(line)
-    if line == "ATTENTION! Les constantes de reaction doivent etre positif":
+    #print(ref)
+    if line == ref:
         print(" ------------------------             test OK  --------------------------------")
     else:
         print(" ------------------------             test KO  --------------------------------")
